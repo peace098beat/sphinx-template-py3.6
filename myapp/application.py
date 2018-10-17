0fe51bbb1ba06ba03c19601bee1af0aa47a0e8d2
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2018-10-17 18:24:54 +0900

    Add files via upload

---
+++ myapp/application.py
@@ -0,0 +1,19 @@
+class App:
+    """Summary line.
+    """
+
+    def sum(self, x, y):
+        """sum
+
+        Args:
+            x (int): 1st argument
+            y (int): 2nd argument
+
+        Returns:
+            int: sum result
+
+        Examples:
+            >>> print(testfunc(2,5))
+            7
+        """
+        return x + y
